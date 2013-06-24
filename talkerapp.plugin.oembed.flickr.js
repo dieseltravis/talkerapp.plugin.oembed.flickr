446728b206328f0d7bc5cb93d3bf0664cf941f07
Travis <hardiman@brandlogic.com>
2013-06-24 17:41:25 -0400

    start debugging and test in jsbin

---
+++ talkerapp.plugin.oembed.flickr.js
@@ -0,0 +1,28 @@
+plugin.onMessageInsertion = function (event) {
+  var flickr_status_expression = /^https?:\/\/(?:(?:(?:\w*\.)?flickr.com\/photos)|(?:flic\.kr\/p))\/\S+/i,
+      last_anchor = Talker.getLastInsertion().find('a'),
+      last_href = last_anchor.attr('href') || '';
+
+  if (flickr_status_expression.test(last_href)) {
+    var id = last_href.match(flickr_status_expression)[0],
+        //TODO: get JSONP since JSON wouldn't work across domain
+        url = 'http://www.flickr.com/services/oembed?format=json&url=' + window.encodeURIComponent(id) + '&callback=?';
+    
+    if (last_anchor.hasClass('transformed')) {
+      return true; // Do not transform the link a second time.
+    }
+    
+    $.getJSON(url, function (data) {
+      if (data && !last_anchor.hasClass('transformed')) {
+        var html = "<a href='{web_page_short_url}'><h2>{title}</h2><img src='{thumbnail_url}' width='{thumbnail_width}' height='{thumbnail_height}' /><h3>{license} - {author_name}</h3></a>".replace(/\{(\w+)\}/gi, function (subString, group1 /* , offset, inputString */ ) {
+			return data[group1];
+		});
+
+        // tag as tranformed so we don't do it again
+        last_anchor.addClass('transformed')
+          .fadeOut('slow')
+          .after($("<div class='travi-flickr'>" + html + "</div>"));
+      }
+    });
+  }
+};
